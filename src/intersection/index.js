8c8e8d4eab8c7e2bbc6e8f45c3da020e75bb9116
Logan Pritchett <logan.pritchett01@gmail.com>
2024-05-20 23:39:52 -0500

    chore: Refactor intersection function to use a more efficient solution

--- src/intersection/index.js
+++ src/intersection/index.js
@@ -1,3 +1,35 @@
-function intersection(a, b) {}
+function intersection(a, b) {
+  /*
+? CHEGG RECOMMENDED SOLUTION 1
+  let result = [];
+
+  for (let element of a) {
+    if (b.includes(element) && !result.includes(element)) {
+      result.push(element);
+    }
+  }
+
+  return result;
+  
+? CHEGG RECOMMENDED SOLUTION 2
+  let map = new Map();
+  let set = new Set();
+
+  for (let element of a) {
+    map.set(element, true);
+  }
+
+  for (let element of b) {
+    if (map.has(element)) {
+      set.add(element);
+    }
+  }
+
+  return Array.from(set);
+  */
+
+  // Solution from Copilot
+  return [...new Set(a.filter((element) => b.includes(element)))];
+}
 
 module.exports = intersection;
