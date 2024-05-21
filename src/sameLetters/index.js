78e7a3d2065d658c30362c4f33cec60ee4e0dfd7
Logan Pritchett <logan.pritchett01@gmail.com>
2024-05-21 00:04:06 -0500

    chore: Refactor sameLetters and sumPairs functions for improved efficiency

--- src/sameLetters/index.js
+++ src/sameLetters/index.js
@@ -2,6 +2,21 @@
  * return a Map of the unique letters and arrays of words that are made up of those letters
  * @param {array} words An array  of words
  */
-function sameLetters(words) {}
+function sameLetters(words) {
+  // ? CHEGG RECOMMENDED SOLUTION
+  let results = new Map();
+
+  for (let word of words) {
+    let lowercaseWord = word.toLowerCase();
+    let letters = new Set(lowercaseWord.split(""));
+    let key = Array.from(letters).sort().join("");
+    if (results.has(key)) {
+      results.get(key).push(word);
+    } else {
+      results.set(key, [word]);
+    }
+  }
+  return results;
+}
 
 module.exports = sameLetters;
--- src/sumPairs/index.js
+++ src/sumPairs/index.js
@@ -3,6 +3,38 @@
  * @param {Array} A an array of numbers
  * @param {Number} N an integer value
  */
-function sumPairs(A, N) {}
+function sumPairs(A, N) {
+  /*
+  ? CHEGG RECOMMENDED SOLUTION 1
+  let output = [];
+  for (let i = 0; i < A.length; i++) {
+    for (let j = i + 1; j < A.length; j++) {
+      if (i !== j && A[i] + A[j] === N) {
+        output.push([A[i], A[j]]);
+      }
+    }
+  }
+  return output;
+*/
+  // ? CHEGG RECOMMENDED SOLUTION 2
+  let numbers = new Map();
+  let solution = new Map();
+
+  for (let e in A) {
+    numbers.set(A[e], e);
+  }
+
+  for (let e in A) {
+    let diff = N - A[e];
+    if (diff != A[e]) {
+      if (numbers.has(diff)) {
+        let pair = [A[e], diff].sort((a, b) => a - b); // ? sort the pair in ascending order
+        solution.set(pair[0], pair[1]); // ? add the sorted pair to the solution
+      }
+    }
+  }
+
+  return Array.from(solution, ([a, b]) => [a, b]); // ? convert the Map to an array of pairs
+}
 
 module.exports = sumPairs;
